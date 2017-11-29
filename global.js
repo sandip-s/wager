831950e0c1e333b01554e896c6a6c9abb309a6fd
sandip-s <30966249+sandip-s@users.noreply.github.com>
2017-11-29 11:17:51 -0800

    global.js file added

---
+++ global.js
@@ -0,0 +1,18 @@
+module.exports = {
+	charlie: function() {
+		return {name: "Charlie Furrer", image: "Charlie.png", friends: [], wagers: [""]}
+	},
+
+	adam: function() {
+		return {name: "Adam Mosharrafa", image: "Adam.png", friends: [], wagers: [""]}
+	},
+
+	sandip: function() {
+		return {name: "Sandip Srinivas", image: "Sandip.png", friends: [], wagers: [""]}
+	},
+
+	zhiwei: function() {
+		return {name: "Zhiwei Gu", image: "Zhiwei.png", friends: [], wagers: [""]}
+	}, 
+}
+
