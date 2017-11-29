6c944198b8cbc4b74aaf2cb46d44978189e77d06
sandip-s <30966249+sandip-s@users.noreply.github.com>
2017-11-29 13:10:09 -0800

    Added info to global.js

--- global.js
+++ global.js
@@ -1,18 +1,34 @@
 module.exports = {
-	charlie: function() {
-		return {name: "Charlie Furrer", image: "Charlie.png", friends: [], wagers: [""]}
-	},
+	charlie: {fullName: "Charlie Furrer",
+				image: "./images/Charlie.png",
+				friends: [module.exports.sandip, module.exports.zhiwei],
+				wagers: ["(Charlie) completed (Sandip)'s wager to cook family dinner by November 24th.",
+					"(Charlie) completed (Zhiwei)'s wager to go a day without checking Instagram by November 24th.",
+					"(Charlie) completed (Zhiwei)'s wager to get lunch with a professor by November 21st."]
+		},
 
-	adam: function() {
-		return {name: "Adam Mosharrafa", image: "Adam.png", friends: [], wagers: [""]}
-	},
+	adam: {fullName: "Adam Mosharrafa", 
+			image: "./images/Adam.png", 
+			friends: [module.exports.sandip, module.exports.zhiwei],
+			wagers: ["(Adam) completed (Zhiwei)'s wager to do laundry by November 23rd.",
+					"(Adam) completed (Sandip)'s wager to run 3 miles by November 22nd.",
+					"(Adam) failed (Sandip)'s wager to call brothers and parents by November 22nd."]
+		},
 
-	sandip: function() {
-		return {name: "Sandip Srinivas", image: "Sandip.png", friends: [], wagers: [""]}
-	},
+	sandip: {fullName: "Sandip Srinivas", 
+			image: "./images/Sandip.png", 
+			friends: [module.exports.adam, module.exports.zhiwei, module.exports.charlie],
+			wagers: ["(Sandip) failed (Adam)'s wager to finish history paper draft by November 18th.",
+					"(Sandip) completed (Charlie)'s wager to go to the gym by November 17th.",
+					"(Sandip) failed (Zhiwei)'s wager to swim laps by November 16th."]
+		},
 
-	zhiwei: function() {
-		return {name: "Zhiwei Gu", image: "Zhiwei.png", friends: [], wagers: [""]}
-	}, 
+	zhiwei: {fullName: "Zhiwei Gu", 
+			image: "./images/Zhiwei.png", 
+			friends: [module.exports.adam, module.exports.sandip, module.exports.charlie], 
+			wagers: ["(Zhiwei) failed (Sandip)'s wager to try new juice cleanse by November 25th.",
+					"(Zhiwei) completed (Charlie)'s wager to run a half marathon by November 15th.",
+					"(Zhiwei) completed (Adam)'s wager to read a new book by November 14th."]
+		} 
 }
 
